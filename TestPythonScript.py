3c413a20b436aacb1147fb9825008484f799bc1c
Shishir <shishirmax@gmail.com>
2017-11-13 10:35:24 +0530

    new test scripts

--- TestPythonScript.py
+++ TestPythonScript.py
@@ -1,5 +1,26 @@
 import requests
+import json
+api_key = "AIzaSyBEQyAbIVXfGTSbLr_S-HUpSZoxfoBbc5I"
 add = "new delhi"
+url = "https://maps.googleapis.com/maps/api/geocode/json?address=+add+&key=AIzaSyBEQyAbIVXfGTSbLr_S-HUpSZoxfoBbc5I"
+api_response = requests.get(url)
+api_response.status_code
+api_response.headers['content-type']
+api_response.encoding
+api_response.json()
+
+json_r = api_reponse.json()
+json_str = json.dumps(json_r)
+parsed_json = json.loads(json_str)
+#this gives the formatted address
+print(parsed_json['results'][0]['formatted_address'])
+#this gives the latitude
+print(parsed_json['results'][0]['geometry']['location']['lat'])
+#this gives the longitude
+print(parsed_json['results'][0]['geometry']['location']['lng'])
+
+
+
 r = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address=
 "+add+"&key=AIzaSyAnkNz_LwkyLkH5eosMfJNMNn0CZCcSCyU")
 r.status_code
